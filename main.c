5ee189532a13e81570b898dfc00c378ba8321d17
richardm1234 <richard.mayer@tum.de>
2026-02-28 14:41:01 +0100

    Semester 1 on screen

--- main.c
+++ main.c
@@ -8,23 +8,19 @@
 int main() {
     SDL_InitSubSystem(SDL_INIT_VIDEO);
     TTF_Init();
-    TTF_Font *font = TTF_OpenFont("arial.ttf", 24);
+    TTF_Font *font = TTF_OpenFont("font/Roboto-VariableFont_wdth,wght.ttf", 24);
     SDL_Window *window = SDL_CreateWindow("GRADES", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 640, 0);
     if (!window) return 1;
     
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) return 1;
 
-    
-    
-    
-
-    SDL_Color white = {0, 0, 0, 255};
+    SDL_Color white = {255, 255, 255, 255};
     SDL_Surface *tempSurface = TTF_RenderText_Blended(font, "Semester 1", white);
     SDL_Texture *text = SDL_CreateTextureFromSurface(renderer, tempSurface);
     SDL_FreeSurface(tempSurface);
 
-    SDL_Rect rect = {0, 0, 300, 300};
+    SDL_Rect rect = {0, 0, 600, 600};
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
