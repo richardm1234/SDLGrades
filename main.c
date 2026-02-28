641c10b0bba8926f549c521ec4a982fa0d3fc761
richardm1234 <richard.mayer@tum.de>
2026-02-28 13:29:07 +0100

    let window open for 10 seconds

--- main.c
+++ main.c
@@ -4,8 +4,11 @@
 
 int main() {
     SDL_InitSubSystem(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Pong Game 1972", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, 0);
+    SDL_Window *window = SDL_CreateWindow("GRADES", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, 0);
     // SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
     SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+    SDL_Delay(10000);
+    
 }
 
