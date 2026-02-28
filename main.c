68580ec90fd80417bd6a07bad5d603a170b953f9
richardm1234 <richard.mayer@tum.de>
2026-02-28 13:59:40 +0100

    draw red square

--- main.c
+++ main.c
@@ -2,13 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main() {
     SDL_InitSubSystem(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("GRADES", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 640, 0);
-    // SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    SDL_Surface *surface = SDL_GetWindowSurface(window);
-
-    SDL_Delay(10000);
+    SDL_Window *window = SDL_CreateWindow("GRADES", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 640, 0);
+    if (!window) return 1;
     
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) return 1;
+
+    SDL_Rect *rect = malloc(sizeof(SDL_Rect));
+    if (!rect) return 1;
+
+    rect->x = 0;
+    rect->y = 0;
+    rect->w = 10;
+    rect->h = 10;
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, rect);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(10000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;    
 }
 
