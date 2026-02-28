95aee3c5c2a802ab325bb9dcea86324ebf3c3618
richardm1234 <richard.mayer@tum.de>
2026-02-28 14:33:42 +0100

    fonts

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CC=gcc
 TARGET=main
 
 all:
-	$(CC) main.c -o $(TARGET) -lSDL2
+	$(CC) main.c -o $(TARGET) -lSDL2 -lSDL2_ttf
 
 clean: 
 	rm $(TARGET)
--- main.c
+++ main.c
@@ -1,30 +1,41 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void initRect(SDL_Rect *rect, int x, int y, int w, int h) {
-    rect->x = x;
-    rect->y = y;
-    rect->w = w;
-    rect->h = h;
-}
+
 
 int main() {
     SDL_InitSubSystem(SDL_INIT_VIDEO);
+    TTF_Init();
+    TTF_Font *font = TTF_OpenFont("arial.ttf", 24);
     SDL_Window *window = SDL_CreateWindow("GRADES", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 640, 0);
     if (!window) return 1;
     
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) return 1;
 
-    SDL_Rect *rect = malloc(sizeof(SDL_Rect));
-    if (!rect) return 1;
-    initRect(rect, 0, 0, 100, 100);
     
+    
+    
+
+    SDL_Color white = {0, 0, 0, 255};
+    SDL_Surface *tempSurface = TTF_RenderText_Blended(font, "Semester 1", white);
+    SDL_Texture *text = SDL_CreateTextureFromSurface(renderer, tempSurface);
+    SDL_FreeSurface(tempSurface);
+
+    SDL_Rect rect = {0, 0, 300, 300};
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderFillRect(renderer, rect);
+    SDL_RenderFillRect(renderer, &rect);
+    SDL_RenderCopy(renderer, text, NULL, &rect);
     SDL_RenderPresent(renderer);
-    SDL_Delay(10000);
+
+    
+    SDL_Delay(3000);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
